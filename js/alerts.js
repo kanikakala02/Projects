e13f847fcdddf57e714e40bb865db136ed1d44a5
kanikakala02 <72757793+kanikakala02@users.noreply.github.com>
2022-12-05 23:26:57 +0530

    Delete js directory

--- js/alerts.js
+++
@@ -1,9 +0,0 @@
-function showdiv()
-{
-    document.getElementById("alert").style.visibility="hidden";
-    document.getElementById("alert").style.display="none";
-}
-
-setTimeout("showdiv()",4000);
-
-
--- js/resp.js
+++
@@ -1,8 +0,0 @@
-burger=document.querySelector('.burger')
-navbar=document.querySelector('.navbar')
-navlist=document.querySelector('.navlist')
-
-burger.addEventListener('click',()=>{
-    navlist.classList.toggle('v-class-resp');
-    navbar.classList.toggle('h-nav-resp');
-})
